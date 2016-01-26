00e6c7fddd175a6e412691dd4a525defc960d817
zeus <thanhphuong.ta@trueplus.vn>
2016-01-26 14:26:26 +0700

    Commit code phan fix kieu du lieu database

--- app/code/local/Magestore/Campaign/Block/Adminhtml/Popup/Edit/Tab/Position.php
+++ app/code/local/Magestore/Campaign/Block/Adminhtml/Popup/Edit/Tab/Position.php
@@ -64,9 +64,12 @@ class Magestore_Campaign_Block_Adminhtml_Popup_Edit_Tab_Position extends Mage_Ad
             'label'		=> Mage::helper('campaign')->__('How many vertical px:'),
             'required'	=> false,
             'name'		=> 'vertical_px',
+            'value'     => '100',
             'note'      => 'Fill px number vertical.',
         ));
-
+        if($data['vertical_px'] == NULL){$data['vertical_px'] = 100;}
+        if($data['horizontal_px'] == NULL){$data['horizontal_px'] = 100;}
+            //var_dump($data); die('ddd');
         $form->setValues($data);
 
         $this->setForm($form);
