5cf0d0ac55e6e4ee23423fd680feb879708e7ab9
Luna <hoangthamhtth@gmail.com>
2016-01-27 08:33:50 +0700

    Edit attribute follow database

--- skin/frontend/base/default/js/campaign/genneral.js
+++ skin/frontend/base/default/js/campaign/genneral.js
@@ -1,3 +1,27 @@
-function addStylemodal(name, age) {
+var Popup = {
+    idPopup: "",
+    effect: "",
+    appear: "",
+    borderSize: "",
+    borderColor: "",
+    paddingSize: "",
+    width: "",
+    bgColor: "",
+    overlayColor: "",
+    closeStyle: "",
+    cornerRadius: "",
+    showPopup: function () {
+
+    },
+    hidePopup: function () {
+
+    },
+    runEffect: function () {
+
+    },
+    showPosition: function () {
+
+    },
+
 
 }
