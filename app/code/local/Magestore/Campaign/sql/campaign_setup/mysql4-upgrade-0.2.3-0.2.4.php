ade51cfd70fa036b8363b6e8d47930c0d19fbdff
Luna <hoangthamhtth@gmail.com>
2016-02-01 17:14:17 +0700

    change text content follow tommy update file

--- app/code/local/Magestore/Campaign/sql/campaign_setup/mysql4-upgrade-0.2.3-0.2.4.php
+++ app/code/local/Magestore/Campaign/sql/campaign_setup/mysql4-upgrade-0.2.3-0.2.4.php
@@ -32,12 +32,6 @@ $installer->run("
     ALTER TABLE {$this->getTable('campaign/template')}
         ADD COLUMN `trigger_popup` VARCHAR(20) NULL;
 
-    ALTER TABLE {$this->getTable('newsletter_subscriber')}
-        DROP `campaign_id`,
-        DROP `campain_name`;
-    ALTER TABLE {$this->getTable('newsletter_subscriber')}
-        add `campaign_id` int(11) NULL default 0,
-        add `campain_name` varchar(255) NULL default ''
 
 ");
 
